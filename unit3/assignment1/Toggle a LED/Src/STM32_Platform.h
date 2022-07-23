95d439f10677df6ce04732e329ff1e908dda0ce6
BishoyRefaatGaber <104644986+BishoyRefaatGaber@users.noreply.github.com>
2022-07-23 10:52:47 +0200

    Update STM32_Platform.h

--- unit3/assignment1/Toggle a LED/Src/STM32_Platform.h
+++ unit3/assignment1/Toggle a LED/Src/STM32_Platform.h
@@ -6,6 +6,7 @@ typedef unsigned char            uint_8;
 typedef unsigned short int       uint_16;
 typedef unsigned int             uint_32;
 typedef unsigned long long int   uint_64;
+/*non-volatile real*/
 typedef float           ufloat_32;
 typedef double          udouble_64;
 typedef long double     uldouble_80;
@@ -17,9 +18,7 @@ typedef char            sint_8;
 typedef short int       sint_16;
 typedef int             sint_32;
 typedef long long int   sint_64;
-typedef float           sfloat_32;
-typedef double          sdouble_64;
-typedef long double     sldouble_80;
+
 
 
 /*volatile unsigned*/
@@ -27,6 +26,10 @@ typedef volatile unsigned char            vuint_8;
 typedef volatile unsigned short int       vuint_16;
 typedef volatile unsigned int             vuint_32;
 typedef volatile unsigned long long int   vuint_64;
+
+
+
+/* volatile real */
 typedef volatile float           vufloat_32;
 typedef volatile double          vudouble_64;
 typedef volatile long double     vuldouble_80;
@@ -38,9 +41,7 @@ typedef volatile char            vsint_8;
 typedef volatile short int       vsint_16;
 typedef volatile int             vsint_32;
 typedef volatile long long int   vsint_64;
-typedef volatile float           vsfloat_32;
-typedef volatile double          vsdouble_64;
-typedef volatile long double     vsldouble_80;
+
 
 
 #endif
